8b3a77ba425261f7ce629c0aa225614fee44f0b3
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-11-24 23:09:52 +0000

    created a function that returns key

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, objectValue){
+// loop though the object
+for(const key in object){
+  //if the objectValue is the same as key 
+  if (objectValue === object[key]){
+
+    // return key
+    return key;
+  }
+}
+//if no key with value found return undefined
+return undefined
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

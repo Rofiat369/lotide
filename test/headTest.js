aa8bd492284ecab8b6b63aa5fd4250ee8d8224d3
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-11-29 13:09:15 +0000

    Used Mocha and chai to update the test code in headtest

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,25 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const head = require("../head");
+const assert = require("chai").assert;
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 'lighthouse Labs' for ['Lighthouse Labs' , 'Bootcamp']", () => {
+    assert.strictEqual(
+      head(["Lighthouse Labs", "Bootcamp"]),
+      "Lighthouse Labs"
+    );
+  });
+  it("returns 1 for [1, 1]", () => {
+    assert.strictEqual(head([1, 1]), 1);
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

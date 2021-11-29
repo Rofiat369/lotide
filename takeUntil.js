b808e6b0b226f86ec7e5924fbedd6c0f6f2f642f
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-11-29 11:52:33 +0000

    refactored code and use file exports instead of copying and pasting the code

--- takeUntil.js
+++ takeUntil.js
@@ -2,7 +2,7 @@ const takeUntil = function(array, callback){
   const results = []; //create an empty array for for the new array
   for (let arr of array){ //loop through every character in the array
     // for every character in the array we want to check if it meets the callback condition
-    if ( callback(arr) === false){ //if it does meet the callback condition //which is one less than 0 or equals to a comma
+    if ( callback(arr) !== true){ //if it does meet the callback condition //which is one less than 0 or equals to a comma
       results.push(arr); //if condition is false push it to the new array
     } else { //if it does meet the callback arguement
       break; //stop looping and return the result

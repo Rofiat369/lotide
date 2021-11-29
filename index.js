126f67910bbdd12911eafb8e18df73cd0b36905c
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-11-29 14:54:45 +0000

    shortened index.js file using the ES6 shorthand

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const assertArrayEqual = require('./assertArrayEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectEqual = require('./assertObjectEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const head   = require('./head');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const middle = require('./middle');
-const pigLatin = require('./pig-latin');
-const reverse = require('./reverse');
-const tail   = require('./tail');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
+const assertArrayEqual = require("./assertArrayEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectEqual = require("./assertObjectEqual");
+const countLetters = require("./countLetters");
+const countonly = require("./countonly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const head = require("./head");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const middle = require("./middle");
+const pigLatin = require("./pig-latin");
+const reverse = require("./reverse");
+const tail = require("./tail");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 module.exports = {
-  assertArrayEqual: assertArrayEqual,
-  assertEqual: assertEqual,
-  assertObjectEqual: assertObjectEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  head:   head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  pigLatin: pigLatin,
-  reverse: reverse,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without
+  assertArrayEqual,
+  assertEqual,
+  assertObjectEqual,
+  countLetters,
+  countonly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  map,
+  middle,
+  pigLatin,
+  reverse,
+  tail,
+  takeUntil,
+  without,
 };
